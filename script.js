ed53f5c540c2aceb1ecb5e2252f97b72322f0c03
Nayeem7421 <139427572+Nayeem7421@users.noreply.github.com>
2025-04-04 19:56:42 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+```javascript
+const menuItems = document.querySelectorAll('.menu li a');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        menuItems.forEach(i => i.classList.remove('active'));
+        item.classList.add('active');
+    });
+});
+```
